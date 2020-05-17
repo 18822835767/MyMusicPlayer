c578799071e6a801408f176dd975084ae55217ba
18822835767 <1165407654@qq.com>
2020-05-17 20:02:56 +0800

    写好了HomePage的presenter类

---
+++ app/src/main/java/com/example/www11/mymusicplayer/presenter/HomePagePresenterImpl.java
@@ -0,0 +1,27 @@
+package com.example.www11.mymusicplayer.presenter;
+
+import com.example.www11.mymusicplayer.contract.HomePageContract;
+import com.example.www11.mymusicplayer.model.HomePageModelImpl;
+
+import java.util.List;
+
+public class HomePagePresenterImpl implements HomePageContract.HomePagePresenter,
+        HomePageContract.OnHomePageListener {
+    private HomePageContract.HomePageModel mHomePageModel;
+    private HomePageContract.OnHomePageView mOnHomePageView;
+
+    public HomePagePresenterImpl(HomePageContract.OnHomePageView onHomePageView){
+        mOnHomePageView = onHomePageView;
+        mHomePageModel = new HomePageModelImpl();
+    }
+    
+    @Override
+    public void getBanner() {
+        mHomePageModel.getBanner(this);
+    }
+
+    @Override
+    public void onSuccess(List<String> bannerUrl) {
+        mOnHomePageView.showBanner(bannerUrl);
+    }
+}
